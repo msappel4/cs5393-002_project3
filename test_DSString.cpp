4a721d85cd79fcc49aeca9defd5cc2a1ba1bbd1d
msappel4 <167126884+msappel4@users.noreply.github.com>
2024-10-30 10:42:43 -0500

    Update test_DSString.cpp

--- test_DSString.cpp
+++ test_DSString.cpp
@@ -1,10 +1,9 @@
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include "DSString.h"
-
-/* TODO: This file is a start to test your DSString class. Add more tests and add code to check if the results are correct. */
 
 int main()
 {  
@@ -15,26 +14,34 @@ int main()
     myString = "Good bye!";
     std::cout << myString << "\n";
 
-    std::cout << "substr: " << myString.substring(5, 3) << "\n";
+ std::cout << "substr: " << myString.substring(5, 3) << "\n";
 
-    // test some operators (=, ==, [])
     DSString a = "test";
-    DSString b;
-    b = a;
+   DSString b = a;
 
     std::cout << std::boolalpha;
     std::cout << (a == b) << "\n";
+  
+/////////////////test
+DSString tester = "this is a test with a string of words!";
+ std::vector<DSString> words = tester.tokenizeDSString();
 
-    b[0] = 'T';
-    std::cout << "a is now: " << a << "\n";
-    std::cout << (a == b) << "\n";
+for(int i = 0; i < words.size(); i++){
+ std:: cout << words.at(i) << "\n";
+}
+
+
+
+
+
+////////end test 
 
     // use initialization list
     std::vector<DSString> strings = {
-        DSString("bb"),
+        DSString("bbb"),
         DSString("aaa"),
         DSString("ddd"),
-        DSString("ee"),
+        DSString("eee"),
         DSString("ccc")};
 
     // find strings
@@ -42,20 +49,36 @@ int main()
         std::cout
             << s << "\n";
 
-    // find implements linear search
-    std::cout << "found ddd: " << (std::find(strings.begin(), strings.end(), DSString("ddd")) != strings.end()) << "\n";
-    std::cout << "found z: " << (std::find(strings.begin(), strings.end(), DSString("z")) != strings.end()) << "\n";
+     /*
+       test to see if the + operator works
+       a = "hello";
+       b = "there";
+       std:: cout << "hi \n" ;
+       std::cout << a << " \n"; 
+       std::cout << b << " \n"; 
+       std::cout << (a + b) << " \n"; 
+       std :: cout << "hi";
+     */
 
-    // sorting using the STL (note thus needs operator= and operator<)
+    std::cout << "found ddd: " << (std::find(strings.begin(), strings.end(), DSString("ddd")) != strings.end()) << "\n";
+    std::cout << "found zzz: " << (std::find(strings.begin(), strings.end(), DSString("zzz")) != strings.end()) << "\n";
+
+    // sorting using the STL
     std::sort(strings.begin(), strings.end());
 
     for (const auto &s : strings)
         std::cout
             << s << "\n";
 
-    // the data structure is sorted. Now we can do more efficient search using STL binary search
+    // now we can do more efficient search using STL binary search
     std::cout << "found ddd: " << binary_search(strings.begin(), strings.end(), DSString("ddd")) << "\n";
-    std::cout << "found z: " << binary_search(strings.begin(), strings.end(), DSString("z")) << "\n";
+    std::cout << "found zzz: " << binary_search(strings.begin(), strings.end(), DSString("zzz")) << "\n";
+   
 
-    return 0;
+
+
+   /////////// put the table in token highkey confused 
+
+
+    return 0; 
 }
